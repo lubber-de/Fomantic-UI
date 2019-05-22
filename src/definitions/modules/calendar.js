47b93f0abab21ad611ba38a3a9ca289dfa602170
lubber-de <c64@lubber.de>
2019-05-22 19:13:55 +0200

    feat(calendar): added a before event to the prev/next buttons

--- src/definitions/modules/calendar.js
+++ src/definitions/modules/calendar.js
@@ -512,7 +512,9 @@ $.fn.calendar = function(parameters) {
                 var forceSet = target.hasClass(className.today);
                 module.selectDate(date, forceSet);
               } else if (focusDate) {
-                module.set.focusDate(focusDate);
+                if(settings.onBeforeMonthChange.call(element, focusDate) !== false) {
+                  module.set.focusDate(focusDate);
+                }
               } else if (mode) {
                 module.set.mode(mode);
               }
@@ -1525,6 +1527,10 @@ $.fn.calendar.settings = {
 
   // callback when a event label is clicked
   onEventSelect: function (date, label) {
+  },
+
+  // callback when a prev or next button is clicked before the new month gets rendered
+  onBeforeMonthChange: function (date) {
   },
 
   // is the given date disabled?
