43396f38e52ca33117fb142e965757c07c658360
lubber-de <c64@lubber.de>
2022-01-22 22:57:31 +0100

    feat(nightly): update node-fetch to 3

--- scripts/nightly-version.js
+++ scripts/nightly-version.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const path = require('path')
 
 // npm
-const fetch = require('node-fetch')
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const semver = require('semver')
 
 // pkg
